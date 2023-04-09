c5c66fd28c101e1713537c609791e7c5a29b2abf
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-04-09 10:02:46 -0700

    fix statements

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -22,23 +22,21 @@ def api():
 
     # Todo list for the given user
     todo_response = requests.get(f"{API_URL}/todos").json()
-
+    
     # Create a dictionary for all users
     dictionary_user = {}
-    
+
     for task in todo_response:
         user_id = task['userId']
-        
-        if user_id in dictionary_user:
-            dictionary_user[user_id].append({
-                "username": next(user[
-                    'username'] for user in user_response if user['id'] == user_id),
-                "task": task['title'],
-                "completed": task['completed']
-            })
-
-        else:
+ 
+        if user_id not in dictionary_user:
             dictionary_user[user_id] = []
+        dictionary_user[user_id].append({
+            "username": next(user[
+                'username'] for user in user_response if user['id'] == user_id),
+            "task": task['title'],
+            "completed": task['completed']
+        })
 
     # Write to JSON file
     with open("todo_all_employees.json", 'w') as jsonfile:
