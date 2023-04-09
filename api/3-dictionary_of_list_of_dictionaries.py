b00f579599eaeb6573f75c20faa5fa71879b1cf6
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-04-09 10:05:06 -0700

    PEP8 validation

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -22,18 +22,18 @@ def api():
 
     # Todo list for the given user
     todo_response = requests.get(f"{API_URL}/todos").json()
-    
+
     # Create a dictionary for all users
     dictionary_user = {}
 
     for task in todo_response:
         user_id = task['userId']
- 
+
         if user_id not in dictionary_user:
             dictionary_user[user_id] = []
         dictionary_user[user_id].append({
             "username": next(user[
-                'username'] for user in user_response if user['id'] == user_id),
+            'username'] for user in user_response if user['id'] == user_id),
             "task": task['title'],
             "completed": task['completed']
         })
